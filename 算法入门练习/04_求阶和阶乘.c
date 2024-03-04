2220a085a3960e9a4766eb8c310335569093b0bb
CoffeeScholar <76543969+coffeescholar@users.noreply.github.com>
2024-03-04 18:58:37 +0800

    更新 04_求阶和阶乘.c

--- 算法入门练习/04_求阶和阶乘.c
+++ 算法入门练习/04_求阶和阶乘.c
@@ -19,17 +19,34 @@ int calculateFactorial(int n) {
     return factorial;
 }
 
+// 计算 n 的阶和
+int calculateSum_Recursive(int n) {
+    if (n == 1) { // 1 的阶和为 1，终止递归
+        return 1;
+    } else {
+        return n + calculateSum_Recursive(n - 1); // 递归调用自己，求 n-1 的阶和
+    }
+}
+
+// 计算 n 的阶乘
+int calculateFactorial_Recursive(int n) {
+    if (n == 0) {   // 0 的阶乘为 1，终止递归
+        return 1;
+    } else {
+        return n * calculateFactorial_Recursive(n - 1); // 递归调用自己，求 n-1 的阶乘
+    }
+}
+
 int main() {
     // 设置控制台输出为 UTF-8 编码
     system("chcp 65001>nul");
 
     int n = 5;
-    int sum = calculateSum(n);
-    int factorial = calculateFactorial(n);
-
     // 输出结果
-    printf("5 的阶和为：%d\n", sum);
-    printf("5 的阶乘为：%d\n", factorial);
+    printf("5 的阶和为：%d\n", calculateSum(n));
+    printf("5 的阶乘为：%d\n", calculateFactorial(n));
+    printf("5 的阶和为（递归）：%d\n", calculateSum_Recursive(n));
+    printf("5 的阶乘为（递归）：%d\n", calculateFactorial_Recursive(n));
 
     return 0;
 }

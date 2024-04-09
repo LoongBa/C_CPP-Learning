4705a94a8894193cfbd161db45d9d0fe7427512e
CoffeeScholar <76543969+coffeescholar@users.noreply.github.com>
2024-04-09 19:36:20 +0800

    调整内容

--- 算法入门练习/hello.c
+++ 算法入门练习/hello.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
-void main() {
-    printf("Hello World");
+#include <stdlib.h>
+//#include <fcntl.h>
+
+int main() {
+    system("chcp");
+    //system("pause");
+    system("chcp 65001>nul");
+
+    // 更改控制台输出模式为宽字符模式
+    //_setmode(_fileno(stdout), _O_U16TEXT);
+    printf("Hello World！你好，我来了！");
 }
